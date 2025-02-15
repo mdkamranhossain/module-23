e39e28eef7f7c545710629fcdb24b4b2748bb759
Kamran Hossain <you@example.com>
2025-02-15 19:16:29 +0600

    task-5

---
+++ task-5.js
@@ -0,0 +1,17 @@
+const employees = [
+    { name: "shahin", experience: 5, starting: 20000, increment: 5000 },
+    { name: "shihab", experience: 3, starting: 15000, increment: 7000 },
+    { name: "shikot", experience: 9, starting: 30000, increment: 1000 },
+    { name: "shohel", experience: 0, starting: 29000, increment: 4000 },
+];
+
+function totalSalary(employees){
+    let totalSalary = 0;
+    for(const employe of employees){
+        const currentSalary = (employe.experience * employe.increment )+ employe.starting;
+        totalSalary += currentSalary;
+    }
+    return totalSalary;
+
+}
+console.log(totalSalary(employees));
